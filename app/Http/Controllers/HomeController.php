22a97d63a57082652ebdf8fc52abb12c2200a2c1
shubhamwadhwa711 <shubhamwadhwa711@gmail.com>
2021-12-10 18:03:27 +0530

    Login auth set

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Redirect;
 class HomeController extends Controller
 {
     //Index Page
-    public function index(){
-        return view('home.index',['title'=>'Home']);
+    public function index(Request $request){
+        $value = $request->session()->get('user');
+        if($value != null){
+            return view('home.index',['title'=>'Home']);
+        }
+        else{
+            return Redirect::to('/login');
+        }
+        
     }
 
     //Login Page
     public function Login(){
         return view('login.index',['title'=>'Login']);
+        
     }
+
+    public function Logout(Request $request){
+        $request->session()->forget('user');
+        return response()->json(['success'=>'Successfully!','redirect'=>'/login']);
+    }
+    //Login
+    public function LoginFunc(Request $request){
+        $input = $request->all();
+        $usreEmail = env('USER_LOGIN_NAME');
+        $userPassword = env('USER_LOGIN_PASSWORD');
+        if($input['email'] === $usreEmail && $input['password'] === $userPassword){
+            $request->session()->put('user','Virat Gandhi');
+            return response()->json(['success'=>'Data Successfully Added!','redirect'=>'/dashboard']);
+        }
+        else{
+            return response()->json(['data'=> $input,'message'=>'please check details!','error'=>true]);
+        }
+        
+    }
+    //Add And Edit GST
+    public function AddEditGST(Request $request){
+        $input = $request->all();
+        return response()->json(['data'=> $input, 'success'=>'Data Successfully Added!']);
+    }
+
+     //Edit Account Details
+     public function EditAccountDetails(Request $request){
+        $input = $request->all();
+        return response()->json(['data'=> $input, 'success'=>'Data Successfully Added!']);
+    }
+    
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,63 @@
-jQuery(document).ready(function(){
-    console.log("Hello");
+jQuery(document).ready(function($){
+    
+    //Setup Token in Ajax Call
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    var AjaxUrl = {
+        editGSTform:'/add-edit-gst',
+        editAccountDetailsform:'/edit-acount-details',
+        userLoginform:'/login'
+    };
+    async function isEmptyForm(id){
+        var formid = '#'+id+'';
+        var formUrlName = id.replace(/\-/g, '');
+        var formData = $(formid).serializeArray();
+        var formMethod = $(formid).attr('method');
+        ajaxSendData(formid,formData,AjaxUrl[formUrlName],formMethod)
+    }
+    function ajaxSendData(formid,formData, formUrl,formMethod){
+        if(!$(formid).find('.form-group-err').hasClass('d-none')){
+            $(formid).find('.form-group-err').addClass('d-none');
+            $(formid).find('.form-group-err').text("");
+        }
+        $.ajax({
+            url: formUrl,
+            type: formMethod,
+            data:formData,
+            success: function(data) {
+                console.log(data);
+                if(data.redirect != undefined){
+                    var rdurl = window.location.origin+data.redirect;
+                    window.location.replace(rdurl);
+                }
+                else if(data.error != undefined){
+                    $(formid).find('.form-group-err').removeClass('d-none');
+                    $(formid).find('.form-group-err').text(data.message)
+                }
+            },
+        });
+    }
+    $('#userLogout').on('click',function(e){
+        e.preventDefault();
+        $.ajax({
+            url: '/logout',
+            type: 'POST',
+            data:'logout',
+            success: function(data) {
+                if(data.redirect != undefined){
+                    var rdurl = window.location.origin+data.redirect;
+                    window.location.replace(rdurl);
+                }
+            },
+        });
+    })
+    $(document).on('click', '.submit-btn-form', function(e){
+        e.preventDefault();
+        var formID = $(this).attr('data-form-id');
+        isEmptyForm(formID); 
+    })
+
 })
